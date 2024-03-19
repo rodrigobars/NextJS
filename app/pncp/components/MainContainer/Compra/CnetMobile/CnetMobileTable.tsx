4596a0985a099adeb23b55397edc31aadf465cc5
rodrigo <rodrigobs@id.uff.br>
2024-03-19 09:30:30 -0300

    up

--- app/pncp/components/MainContainer/Compra/CnetMobile/CnetMobileTable.tsx
+++ app/pncp/components/MainContainer/Compra/CnetMobile/CnetMobileTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Textarea, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Card, CardHeader, CardBody, Divider } from '@nextui-org/react';
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Card, CardHeader, CardBody, Divider, Chip } from '@nextui-org/react';
 import { formatarCnpj } from '@/app/components/Utils/Utils';
 import { formatarParaReais } from '../../../../../components/Utils/Utils';
 
@@ -7,6 +7,7 @@ export default function CnetMobileTable( { dados } ) {
     return (
         <div className='sm gap-5 overflow-x-auto max-w-full'>
             <Table
+                isStriped
                 color='secondary'
                 isHeaderSticky
                 topContentPlacement="outside"
@@ -19,6 +20,7 @@ export default function CnetMobileTable( { dados } ) {
                     <TableColumn>Descrição</TableColumn>
                     <TableColumn>Quantidade</TableColumn>
                     <TableColumn>ValorEstimado(Und)</TableColumn>
+                    <TableColumn>Info</TableColumn>
                     <TableColumn>Empresas</TableColumn>
                 </TableHeader>
                 <TableBody emptyContent={"No rows to display."}>
@@ -34,15 +36,10 @@ export default function CnetMobileTable( { dados } ) {
                                     <p className='font-bold'>{proposta.numero}</p>
                                 </TableCell>
 
-                                <TableCell>
-                                    <Textarea
-                                        isReadOnly
-                                        color='default'
-                                        labelPlacement="outside"
-                                        defaultValue={proposta.descricao}
-                                        maxRows={2}
-                                        className="min-w-[200px] max-w-full text-small text-default-600"
-                                    />
+                                <TableCell
+                                    className='animate-none text-center'
+                                    >
+                                    <p className='font-bold'>{proposta.descricao}</p>
                                 </TableCell>
 
                                 <TableCell
@@ -56,6 +53,29 @@ export default function CnetMobileTable( { dados } ) {
                                     >
                                     <p className='font-bold'>{formatarParaReais(proposta.valorEstimadoUnitario)}</p>
                                 </TableCell>
+
+                                <TableCell
+                                    className='text-left'
+                                    >
+                                    <p><strong>Tipo: </strong>{proposta.tipo}</p>
+                                    <p><strong>disputaPorValorUnitario: </strong>{proposta.disputaPorValorUnitario}</p>
+                                    <p><strong>criterioJulgamento: </strong>{proposta.criterioJulgamento}</p>
+                                    <p><strong>homologado: </strong>{proposta.homologado}</p>
+                                    <p><strong>situacaoEnvioResultado: </strong>{proposta.situacaoEnvioResultado}</p>
+                                    <p><strong>numeroSessaoJulgHab: </strong>{proposta.numeroSessaoJulgHab}</p>
+                                    <p><strong>tipoTratamentoDiferenciadoMeEpp: </strong>{proposta.tipoTratamentoDiferenciadoMeEpp}</p>
+                                    <p><strong>participacaoExclusivaMeEppOuEquiparadas: </strong>{proposta.participacaoExclusivaMeEppOuEquiparadas}</p>
+                                    <p><strong>situacao: </strong>{proposta.situacao}</p>
+                                    <p><strong>fase: </strong>{proposta.fase}</p>
+                                    <p><strong>criterioValor: </strong>{proposta.criterioValor}</p>
+                                    <p><strong>priorizarAbertura: </strong>{proposta.priorizarAbertura}</p>
+                                    <p><strong>julgHabEncerrada: </strong>{proposta.julgHabEncerrada}</p>
+                                    <p><strong>criterioValor: </strong>{proposta.criterioValor}</p>
+                                    <p><strong>qtdeItensDoGrupo: </strong>{proposta.qtdeItensDoGrupo}</p>
+                                    <p><strong>qtdeAceitaSrp: </strong>{proposta.qtdeAceitaSrp}</p>
+                                    <p><strong>qtdeAdjudicadaSrp: </strong>{proposta.qtdeAdjudicadaSrp}</p>
+                                    <p><strong>prazosFaseRecursal: </strong>{proposta.prazosFaseRecursal}</p>
+                                </TableCell>
                                 
                                 <TableCell>
                                     <div className='flex gap-4'>
@@ -65,28 +85,32 @@ export default function CnetMobileTable( { dados } ) {
                                                 .map((item, itemIndex) => (
                                                     <Card key={itemIndex} radius="lg" shadow='md' isPressable
                                                         className='w-[300px] bg-neutral-800'>
-                                                        <CardHeader>
+                                                        <CardHeader className='flex justify-between'>
                                                             <div>
                                                                 <p className="text-small truncate text-green-400 animate-pulse">
                                                                     {item.participante.nome.length > 25 ? `${item.participante.nome.slice(0, 25)}...` : item.participante.nome}
                                                                 </p>
                                                                 <p className="text-small text-default-500 text-left">{formatarCnpj(item.participante.identificacao)}</p>
                                                             </div>
+                                                            <Chip variant='shadow' color='secondary'>{itemIndex+1}.º</Chip>
                                                         </CardHeader>
                                                         <Divider/>
                                                         <CardBody>
                                                             <div>
-                                                                <p>tipo: {item.participante.tipo}</p>
-                                                                <p>declaracaoMeEpp: {item.declaracaoMeEpp}</p>
-                                                                <p>justificativaUltimaSolicitacaoAnexos: {item.justificativaUltimaSolicitacaoAnexos}</p>
-                                                                <p>motivoDesclassificacao: {item.motivoDesclassificacao}</p>
-                                                                <p>Unitario: {formatarParaReais(item.valores.valorPropostaInicialOuLances.valorCalculado.ValorUnitario)}</p>
-                                                                <p>Total: {formatarParaReais(item.valores.valorPropostaInicialOuLances.valorCalculado.valorTotal)}</p>
+                                                                <p><strong>tipo: </strong>{item.participante.tipo}</p>
+                                                                <p><strong>declaracaoMeEpp: </strong>{item.declaracaoMeEpp}</p>
+                                                                <p><strong>quantidade ofertada: </strong>{item.quantidadeOfertada}</p>
+                                                                <p><strong>Unitario: </strong>{item.valores.valorPropostaInicialOuLances.valorCalculado.valorUnitario ? formatarParaReais(item.valores.valorPropostaInicialOuLances.valorCalculado.valorUnitario) : 'None'}</p>
+                                                                <p><strong>Total: </strong>{item.valores.valorPropostaInicialOuLances.valorCalculado.valorTotal ? formatarParaReais(item.valores.valorPropostaInicialOuLances.valorCalculado.valorTotal) : 'None'}</p>
                                                                 {(item.valorNegociado) &&
-                                                                    <p>Valor Negociado: {item.valorNegociado}</p>
+                                                                    <p>Valor Negociado: {formatarParaReais(item.valorNegociado)}</p>
                                                                 }
-                                                                <p>Marca: {item.marcaFabricante}</p>
-                                                                <p>Modelo: {item.modeloVersao}</p>
+                                                                <p><strong>Marca: </strong>{item.marcaFabricante}</p>
+                                                                <p><strong>Modelo: </strong>{item.modeloVersao}</p>
+                                                                <p><strong>descricaoDetalhada: </strong>{item.descricaoDetalhada}</p>
+                                                                <p><strong>motivoDesclassificacao: </strong>{item.motivoDesclassificacao}</p>
+                                                                <p><strong>situacao: </strong>{item.situacao}</p>
+                                                                <p><strong>justificativaUltimaSolicitacaoAnexos: </strong>{item.justificativaUltimaSolicitacaoAnexos}</p>
                                                             </div>
                                                         </CardBody>
                                                     </Card>
